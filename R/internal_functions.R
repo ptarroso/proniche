7a3d04ce0e9aedf98ed923ca22cb448c42a1be48
Pedro Tarroso <ptarroso@cibio.up.pt>
2025-01-09 16:15:12 +0000

    domain an mahalanobis updated

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -161,63 +161,151 @@ kernel_plot <- function(model, cols = 1:2, contours = 10, border = "red",
     }
 }
 
+# Returns Gower's distance
 gower <- function(x, train, rng = apply(train, 2, function(x) diff(range(x)))) {
     p <- ncol(train)
+    G <- 0
     for (k in 1:p) {
         d <- abs(x[k] - train[, k]) / rng[k]
-        if (k == 1) {
-            G <- d
-        } else {
-            G <- G + d
-        }
+        G <- G + d
     }
     G / p
 }
 
-domainmodel <- function(vals, vars) {
-    data <- as.data.frame(vars, na.rm = F)
+igower <- function(model, D) {
+    p <- model$nvars
+    rng <- model$model
+    target <- p * D
+    points <- list()
+
+    for (i in 1:nrow(model$x)) {
+        pnt <- model$x[i, ]
+
+        points[[i]] <- expand.grid(
+            lapply(1:p, function(k) {
+                delta <- target * rng[k]
+                c(pnt[k] - delta, pnt[k] + delta)
+            })
+        )
+    }
+    return(points)
+}
+
+# Based on Carpenter et al. (1993). Biodiversity Conservation 2, 667-680
+domain_model <- function(x) {
+    x <- na.exclude(as.matrix(x))
+    rng <- unlist(apply(x, 2, function(d) diff(range(d))))
+    model <- list(
+        type = "domain",
+        model = rng,
+        x = x,
+        nvars = ncol(x)
+    )
+    class(model) <- "proniche"
+    return(model)
+}
+
+domain_predict <- function(model, newdata = NULL) {
+    if (is.null(newdata)) {
+        data <- as.matrix(model$x)
+    } else {
+        data <- na.exclude(as.matrix(newdata))
+    }
     D <- rep(NA, nrow(data))
-    mask <- !is.na(rowSums(data))
-    rng <- unlist(apply(vals, 2, function(d) diff(range(d))))
-    for (i in which(mask)) {
-        G <- gower(unlist(data[i, ]), vals, rng)
+    for (i in 1:nrow(data)) {
+        G <- gower(unlist(data[i, ]), model$x, model$model)
         G[which(G > 1)] <- 1
         D[i] <- max(1 - G, na.rm = T)
     }
-    pred <- vars[[1]] * NA
-    if (inherits(pred, "SpatRaster")) { # (AMB added)
-        terra::values(pred) <- D
-    } else {
-        pred <- D
-    } # (AMB edited)
-    list(pred)
+    return(D)
 }
 
+domain_plot <- function(model, cols = 1:2, contours = seq(0.9, 1, 0.01),
+                        border = "red", pnt.col = "gray", add = FALSE, ...) {
+    if (!add) {
+        plot(model$x[, cols], col = pnt.col, ...)
+    }
+    if (length(contours) == 1) {
+        contours <- seq(0, 1, length.out = contours + 2)
+        contours <- contours[-c(1, length(contours))]
+    }
+    for (d in contours) {
+        # Using terra to retrive boundaries by aggregation
+        bounds <- terra::vect()
+        points <- igower(model, 1 - d)
+        for (i in 1:length(points)) {
+            rng1 <- range(points[[i]][, cols[1]])
+            rng2 <- range(points[[i]][, cols[2]])
+            e <- terra::ext(rng1[1], rng1[2], rng2[1], rng2[2])
+            bounds <- rbind(bounds, terra::vect(e))
+        }
+        bounds <- terra::aggregate(bounds)
+        terra::lines(bounds, col = border, ...)
+    }
+}
+
+
 # mahalanobis distance
-mah.dist <- function(x, u, sigma) {
+mah_dist <- function(x, u, sigma) {
     sqrt(t(x - u) %*% solve(sigma) %*% (x - u))
 }
 
-# Note: scales distances to zero-one range and inverts (near is 1, far is zero)
-mahalanobisModel <- function(vals, vars) {
-    data <- as.data.frame(vars, na.rm = F)
-    M <- rep(NA, nrow(data))
-    mask <- !is.na(rowSums(data))
-    u <- colMeans(vals)
-    sigma <- stats::cov(vals) # Need always a few points to estimate covaraince
-    for (i in which(mask)) {
-        M[i] <- mah.dist(unlist(data[i, ]), u, sigma)
-    }
-    # values(pred) <- 1 - (M/max(M, na.rm=T))
-    p <- 1 - (M / max(M, na.rm = T)) # (AMB edited)
-    if (inherits(vars, "SpatRaster")) { # (AMB added)
-        pred <- vars[[1]] * NA
-        terra::values(pred) <- p # (AMB edited)
-    } else {
-        pred <- p
-    } # (AMB edited)
-    list(pred)
+ellipse_from_cov <- function(sigma, u, sdev_scale, dims = 1:2, n_points = 100) {
+    eig <- eigen(sigma[dims, dims])
+    vec <- eig$vectors
+    val <- sqrt(eig$values)
+    angles <- seq(0, 2 * pi, length.out = n_points)
+    points <- cbind(cos(angles), sin(angles))
+    points <- (points %*% (diag(val) * sdev_scale)) %*% t(vec)
+    points <- t(t(points) + u[dims])
+    return(points)
 }
+
+
+mahalanobis_model <- function(x) {
+    x <- na.exclude(as.matrix(x))
+    u <- colMeans(x)
+    sigma <- stats::cov(x) # Need always a few points to estimate covaraince
+    model <- list(
+        type = "mahalanobis",
+        model = list(u = u, sigma = sigma),
+        x = x,
+        nvars = ncol(x)
+    )
+    class(model) <- "proniche"
+    return(model)
+}
+
+# Note: scales distances to zero-one range and inverts (near is 1, far is zero)
+mahalanobis_predict <- function(model, newdata = NULL) {
+    if (is.null(newdata)) {
+        data <- as.matrix(model$x)
+    } else {
+        data <- na.exclude(as.matrix(newdata))
+    }
+    M <- rep(NA, nrow(data))
+    for (i in 1:length(M)) {
+        M[i] <- mah_dist(unlist(data[i, ]), model$model$u, model$model$sigma)
+    }
+    p <- 1 - (M / max(M)) # (AMB edited)
+    return(p)
+}
+
+
+# contours are sdevs away from the mean
+mahalanobis_plot <- function(model, cols = 1:2,
+                             contours = c(1, 1.64, 1.96, 2.33),
+                             border = "red", pnt.col = "gray", add = FALSE,
+                             ...) {
+    if (!add) {
+        plot(model$x[, cols], col = pnt.col, ...)
+    }
+    for (sdev in contours) {
+        pnt <- ellipse_from_cov(model$model$sigma, model$model$u, sdev, cols)
+        polygon(pnt[, 1], pnt[, 2], border = border, col = NA, ...)
+    }
+}
+
 
 # Multivariate normal distribution
 dmnorm <- function(x, mu, sigma) {
