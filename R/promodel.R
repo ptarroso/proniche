abd24cba439e5022ac0f668f957dc26ccd79a0aa
Pedro Tarroso <ptarroso@cibio.up.pt>
2025-01-10 15:37:03 +0000

    fixed few bugs

--- R/promodel.R
+++ R/promodel.R
@@ -1,3 +1,4 @@
+#' @export
 promodel <- function(x, method = "bioclim", na.rm = TRUE, dup.rm = FALSE,
                      verbosity = 2, ...) {
     # version 1.3 (3 Jan 2025)
@@ -30,15 +31,22 @@ promodel <- function(x, method = "bioclim", na.rm = TRUE, dup.rm = FALSE,
     return(model)
 }
 
+#' @export
 predict.proniche <- function(model, newdata = NULL) {
     if (inherits(newdata, "SpatRaster")) {
-        data <- as.matrix(newdata[[names(model$proniche$x)]])
+        if (is.null(model$proniche$varnames)) {
+            data <- as.matrix(newdata)
+        } else {
+            data <- as.matrix(newdata[[model$proniche$varnames]])
+        }
     } else {
-        data <- newdata[, names(model$proniche$x), drop = FALSE]
+        if (is.null(model$proniche$varnames)) {
+            data <- as.matrix(newdata)
+        } else {
+            data <- newdata[, model$proniche$varnames, drop = FALSE]
+        }
     }
-
     p <- predict(model$proniche, newdata = data)
-
     if (inherits(newdata, "SpatRaster")) {
         pred <- newdata[[1]]
         pred[][, 1] <- p
@@ -48,10 +56,12 @@ predict.proniche <- function(model, newdata = NULL) {
     return(p)
 }
 
+#' @export
 plot.proniche <- function(model, ...) {
     plot(model[["proniche"]], ...)
 }
 
+#' @export
 print.proniche <- function(model) {
     print(model[["proniche"]])
 }
