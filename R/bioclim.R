425acb31564445389b5d649347a5bbcea6704f5e
ptarroso <ptarroso@cibio.up.pt>
2026-06-04 20:15:10 +0100

    corrected right side of envelope

--- R/bioclim.R
+++ R/bioclim.R
@@ -11,29 +11,29 @@
 #' @export
 #'
 bioclim <- function(x, nq = 10) {
-    qt <- seq(0, 0.5, length.out = nq)
-    env_rect <- matrix(NA, nq, ncol(x) * 2 + 1) # (AMB edited)
-    env_rect[, 1] <- qt
-    colnames(env_rect) <- c(
-        "quantile",
-        paste0(
-            rep(colnames(x), each = 2),
-            c("_min", "_max")
-        )
+  qt <- seq(0, 0.5, length.out = nq)
+  env_rect <- matrix(NA, nq, ncol(x) * 2 + 1) # (AMB edited)
+  env_rect[, 1] <- qt
+  colnames(env_rect) <- c(
+    "quantile",
+    paste0(
+      rep(colnames(x), each = 2),
+      c("_min", "_max")
     )
-    for (i in 1:nq) {
-        rng <- apply(x, 2, quantile, probs = c(qt[i], 1 - qt[i]))
-        env_rect[i, -1] <- as.vector(rng)
-    }
-    model <- list(
-        model = env_rect,
-        x = x,
-        nq = nq,
-        nvars = ncol(x),
-        varnames = colnames(x)
-    )
-    class(model) <- "bioclim"
-    return(model)
+  )
+  for (i in 1:nq) {
+    rng <- apply(x, 2, quantile, probs = c(qt[i], 1 - qt[i]))
+    env_rect[i, -1] <- as.vector(rng)
+  }
+  model <- list(
+    model = env_rect,
+    x = x,
+    nq = nq,
+    nvars = ncol(x),
+    varnames = colnames(x)
+  )
+  class(model) <- "bioclim"
+  return(model)
 }
 
 #' Predict Method for Bioclim
@@ -46,23 +46,23 @@ bioclim <- function(x, nq = 10) {
 #' @return A vector of predictions.
 #' @export
 predict.bioclim <- function(object, newdata = NULL, ...) {
-    if (is.null(newdata)) {
-        data <- object$x
-    } else {
-        data <- as.matrix(newdata)
+  if (is.null(newdata)) {
+    data <- object$x
+  } else {
+    data <- as.matrix(newdata)
+  }
+  pred <- rep(0, nrow(data))
+  nvars <- object$nvars
+  nq <- object$nq
+  for (i in 1:nq) {
+    tmp <- pred * 0
+    rng <- matrix(object$model[i, -1], 2, nvars)
+    for (j in 1:nvars) {
+      tmp <- tmp + (data[, j] >= rng[1, j] & data[, j] <= rng[2, j])
     }
-    pred <- rep(0, nrow(data))
-    nvars <- object$nvars
-    nq <- object$nq
-    for (i in 1:nq) {
-        tmp <- pred * 0
-        rng <- matrix(object$model[i, -1], 2, nvars)
-        for (j in 1:nvars) {
-            tmp <- tmp + (data[, j] >= rng[1, j] & data[, j] < rng[2, j])
-        }
-        pred <- pred + (tmp == nvars)
-    }
-    return(pred)
+    pred <- pred + (tmp == nvars)
+  }
+  return(pred)
 }
 
 
@@ -80,18 +80,18 @@ predict.bioclim <- function(object, newdata = NULL, ...) {
 #' @export
 plot.bioclim <- function(x, cols = 1:2, border = "red",
                          pnt.col = "gray", add = FALSE, ...) {
-    if (!add) {
-        plot(x$x[, cols], col = pnt.col, ...)
-    }
-    for (i in 1:x$nq) {
-        rng <- matrix(x$model[i, -1], 2, x$nvars)
-        rect(rng[1, cols[1]],
-            rng[1, cols[2]],
-            rng[2, cols[1]],
-            rng[2, cols[2]],
-            border = border, ...
-        )
-    }
+  if (!add) {
+    plot(x$x[, cols], col = pnt.col, ...)
+  }
+  for (i in 1:x$nq) {
+    rng <- matrix(x$model[i, -1], 2, x$nvars)
+    rect(rng[1, cols[1]],
+      rng[1, cols[2]],
+      rng[2, cols[1]],
+      rng[2, cols[2]],
+      border = border, ...
+    )
+  }
 }
 
 #' Print Method for Bioclim
@@ -102,5 +102,5 @@ plot.bioclim <- function(x, cols = 1:2, border = "red",
 #' @param ... Additional arguments (currently ignored, included for consistency with the generic).
 #' @export
 print.bioclim <- function(x, ...) {
-    print(paste(class(x), "model with", x$nvars, "variables."))
+  print(paste(class(x), "model with", x$nvars, "variables."))
 }
