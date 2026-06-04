94242a8f21e787b497db9bf59aad9bd1af623859
ptarroso <ptarroso@cibio.up.pt>
2026-06-04 20:17:39 +0100

    added truncate value slot

--- R/convexhull.R
+++ R/convexhull.R
@@ -14,43 +14,44 @@
 
 #' @export
 convexhull <- function(x, ...) {
-    x <- na.exclude(as.matrix(x))
-    original <- x
-    nobs <- nrow(x)
-    nvars <- ncol(x)
-    if (nvars < 2) {
-        stop("Convex hull requires at least 2 variables.")
-    }
-    if (nobs < (nvars + 1)) {
-        stop("Insufficient observations: Number of observations must be at least number of variables + 1.")
-    }
-    var_test <- apply(x, 2, var) < .Machine$double.eps**0.5
-    if(any(var_test)) {
-        stop("Constant variables detected. Each variable must have more than one unique value.")
-    }
-    env_ch <- list()
-    i <- 1
-    id <- 1:nrow(x)
-    while (nrow(x) > nvars) {
-        ch <- try(geometry::convhulln(x, ...))
-        if (inherits(ch, "try-error")) stop('convexhull() error; try removing duplicate input rows,\nor adding options="QJ" to the command.') # (AMB added)
-        # Have to redo the IDs in ch to map to the original matrix
-        attr(ch, "pid") <- matrix(id[ch], nrow(ch), ncol(ch))
-        rm <- unique(as.vector(ch))
-        x <- x[-rm, , drop = FALSE] # (AMB edited)
-        id <- id[-rm]
-        env_ch[[i]] <- ch
-        i <- i + 1
-    }
-    model <- list(
-        model = env_ch,
-        x = original,
-        nch = length(env_ch),
-        nvars = nvars,
-        varnames = colnames(x)
-    )
-    class(model) <- "convexhull"
-    return(model)
+  x <- na.exclude(as.matrix(x))
+  original <- x
+  nobs <- nrow(x)
+  nvars <- ncol(x)
+  if (nvars < 2) {
+    stop("Convex hull requires at least 2 variables.")
+  }
+  if (nobs < (nvars + 1)) {
+    stop("Insufficient observations: Number of observations must be at least number of variables + 1.")
+  }
+  var_test <- apply(x, 2, var) < .Machine$double.eps**0.5
+  if (any(var_test)) {
+    stop("Constant variables detected. Each variable must have more than one unique value.")
+  }
+  env_ch <- list()
+  i <- 1
+  id <- 1:nrow(x)
+  while (nrow(x) > nvars) {
+    ch <- try(geometry::convhulln(x, ...))
+    if (inherits(ch, "try-error")) stop('convexhull() error; try removing duplicate input rows,\nor adding options="QJ" to the command.') # (AMB added)
+    # Have to redo the IDs in ch to map to the original matrix
+    attr(ch, "pid") <- matrix(id[ch], nrow(ch), ncol(ch))
+    rm <- unique(as.vector(ch))
+    x <- x[-rm, , drop = FALSE] # (AMB edited)
+    id <- id[-rm]
+    env_ch[[i]] <- ch
+    i <- i + 1
+  }
+  model <- list(
+    model = env_ch,
+    x = original,
+    nch = length(env_ch),
+    truncate = NULL,
+    nvars = nvars,
+    varnames = colnames(x)
+  )
+  class(model) <- "convexhull"
+  return(model)
 }
 
 #' Predict Method for Convex Hull
@@ -63,19 +64,19 @@ convexhull <- function(x, ...) {
 #' @return A vector of predictions.
 #' @export
 predict.convexhull <- function(object, newdata = NULL, ...) {
-    if (is.null(newdata)) {
-        data <- as.matrix(object$x)
-        mask <- NULL
-    } else {
-        data <- as.matrix(newdata)
-        mask <- is.na(rowSums(data))
-    }
-    pred <- rep(0, nrow(data))
-    for (i in 1:object$nch) {
-        pred <- pred + geometry::inhulln(object$model[[i]], data)
-    }
-    pred[mask] <- NA
-    return(pred)
+  if (is.null(newdata)) {
+    data <- as.matrix(object$x)
+    mask <- NULL
+  } else {
+    data <- as.matrix(newdata)
+    mask <- is.na(rowSums(data))
+  }
+  pred <- rep(0, nrow(data))
+  for (i in 1:object$nch) {
+    pred <- pred + geometry::inhulln(object$model[[i]], data)
+  }
+  pred[mask] <- NA
+  return(pred)
 }
 
 #' Plot Method for Convex Hull
@@ -92,18 +93,18 @@ predict.convexhull <- function(object, newdata = NULL, ...) {
 #' @export
 plot.convexhull <- function(x, cols = 1:2, border = "red",
                             pnt.col = "gray", add = FALSE, ...) {
-    if (!add) {
-        plot(x$x[, cols], col = pnt.col, ...)
-    }
-    # The representation of high dimensional chulls is difficult...
-    # The trick used here is to check which points are within each
-    # chull and generate a 2D chull of those on the user defined dims
-    pred <- predict(x)
-    for (i in 1:x$nch) {
-        pnt <- x$x[which(pred >= i), cols]
-        ch <- chull(pnt)
-        polygon(pnt[ch, ], border = border, col = NA, ...)
-    }
+  if (!add) {
+    plot(x$x[, cols], col = pnt.col, ...)
+  }
+  # The representation of high dimensional chulls is difficult...
+  # The trick used here is to check which points are within each
+  # chull and generate a 2D chull of those on the user defined dims
+  pred <- predict(x)
+  for (i in 1:x$nch) {
+    pnt <- x$x[which(pred >= i), cols]
+    ch <- chull(pnt)
+    polygon(pnt[ch, ], border = border, col = NA, ...)
+  }
 }
 
 #' Print Method for Convex Hull
@@ -114,5 +115,5 @@ plot.convexhull <- function(x, cols = 1:2, border = "red",
 #' @param ... Additional arguments (currently ignored, included for consistency with the generic).
 #' @export
 print.convexhull <- function(x, ...) {
-    print(paste(class(x), "model with", x$nvars, "variables."))
+  print(paste(class(x), "model with", x$nvars, "variables."))
 }
